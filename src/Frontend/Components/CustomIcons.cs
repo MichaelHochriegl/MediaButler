baeabd25c19e869dfccd0ed8ab5c75821275e7cc
MichaelHochriegl <michael@hochriegl.dev>
2026-04-25 16:33:55 +0200

    Replace logo image with custom SVG icon and add `CustomIcons` class for reusable icons

---
+++ src/Frontend/Components/CustomIcons.cs
@@ -0,0 +1,53 @@
+namespace Frontend.Components;
+
+/// <summary>
+/// The <c>CustomIcons</c> class provides a collection of custom SVG path data
+/// strings that can be used for rendering unique icons in the application.
+/// </summary>
+public static class CustomIcons
+{
+    /// <summary>
+    /// Represents the SVG path definition for the MediaButler logo icon.
+    /// </summary>
+    public const string Logo = """
+                               <path fill="#111827"
+                                     stroke="currentColor"
+                                     stroke-width="0.6"
+                                     stroke-linejoin="round"
+                                     stroke-linecap="round"
+                                     d="M2.6 7.7
+                                        C2.6 6.8 3.5 6.3 4.3 6.7
+                                        L9.7 9.3
+                                        C10.4 9.7 10.4 14.3 9.7 14.7
+                                        L4.3 17.3
+                                        C3.5 17.7 2.6 17.2 2.6 16.3
+                                        Z" />
+
+                               <path fill="#111827"
+                                     stroke="currentColor"
+                                     stroke-width="0.6"
+                                     stroke-linejoin="round"
+                                     stroke-linecap="round"
+                                     d="M21.4 7.7
+                                        C21.4 6.8 20.5 6.3 19.7 6.7
+                                        L14.3 9.3
+                                        C13.6 9.7 13.6 14.3 14.3 14.7
+                                        L19.7 17.3
+                                        C20.5 17.7 21.4 17.2 21.4 16.3
+                                        Z" />
+
+                               <path fill="#f59e0b"
+                                     stroke="currentColor"
+                                     stroke-width="0.6"
+                                     stroke-linejoin="round"
+                                     stroke-linecap="round"
+                                     d="M9.7 7.7
+                                        C9.7 6.8 10.7 6.3 11.5 6.8
+                                        L15.8 11.2
+                                        C16.2 11.6 16.2 12.4 15.8 12.8
+                                        L11.5 17.2
+                                        C10.7 17.7 9.7 17.2 9.7 16.3
+                                        Z" />
+                               """;
+    
+}
